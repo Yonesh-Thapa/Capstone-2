5fb45686bb79b49981a832477f05dbd0c8c87b51
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-25 08:15:27 -0400

    some css changes

---
+++ scripts/checkname.js
@@ -0,0 +1,14 @@
+const name = [mike, jerry, sam, lilly]
+
+function checkNames(names){
+    if(newName == "mike"){
+
+    }
+    return newName
+}
+
+function nameFunction(){
+    console.log(name.filter(checkNames))
+
+}
+nameFunction
